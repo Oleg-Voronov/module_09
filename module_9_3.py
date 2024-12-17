a58355a31ff831af0365eb654d27d1a91e8d95ce
Oleg-Voronov <vollmihh@gmail.com>
2024-12-17 21:09:19 +0800

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -2,13 +2,7 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (abs(len(x)-len(y)) for x,y in zip(first,second) if len(x) != len(y))
-second_result = []
-
-for i in range(0,min(len(first),len(second))):
-    if len(first[i]) == len(second[i]):
-        second_result.append(True)
-    else:
-        second_result.append(False)
+second_result = (len(first[i]) == len(second[i]) for i in range(0,min(len(first),len(second))))
 
 print(list(first_result))
 print(list(second_result))
