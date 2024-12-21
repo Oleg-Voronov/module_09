b3732476a28f038db30595e7bf29dd8c562af420
Oleg-Voronov <vollmihh@gmail.com>
2024-12-21 19:00:06 +0800

    Create module_9_7_decor.py

---
+++ module_9_7_decor.py
@@ -0,0 +1,19 @@
+def is_print(func):
+    def wrapper(*args):
+        result = func(*args)
+        if isinstance(result,int):
+            print('Простое')
+        else:
+            print('Сложное')
+        return result
+    return wrapper
+
+@is_print
+def sum_three(n1,n2,n3):
+    return n1+n2+n3
+
+res1 = sum_three(2,3,6)
+print(res1)
+
+res2 = sum_three(2,3,6.5)
+print(res2)
